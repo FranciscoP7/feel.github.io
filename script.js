345a1bebcccd365b8aed0e2f82303dc4fc047e91
FranciscoP7 <139173765+FranciscoP7@users.noreply.github.com>
2024-10-06 15:26:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+// Aguarda as vozes estarem disponíveis
+window.speechSynthesis.onvoiceschanged = function() {
+    const voices = window.speechSynthesis.getVoices();
+    // Aqui você pode listar as vozes se quiser
+    voices.forEach((voice) => {
+        console.log(`${voice.name} (${voice.lang})`);
+    });
+};
+
+document.querySelectorAll('.circular-button').forEach(button => {
+    button.addEventListener('click', function() {
+        const text = this.closest('div').previousElementSibling.textContent;
+        const speech = new SpeechSynthesisUtterance(text);
+        
+        // Altere a voz aqui
+        const voices = window.speechSynthesis.getVoices();
+        const selectedVoice = voices.find(voice => voice.name === 'Google português'); // Altere para a voz desejada
+        if (selectedVoice) {
+            speech.voice = selectedVoice;
+        }
+
+        // Verifica se já está tocando um som
+        if (window.speechSynthesis.speaking) {
+            // Se estiver falando, cancela
+            window.speechSynthesis.cancel();
+        } else {
+            // Se não estiver, fala
+            window.speechSynthesis.speak(speech);
+        }
+    });
+});
+
+
 
